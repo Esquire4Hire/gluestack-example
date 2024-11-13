8e1ff55140147b2fc2818072510b6eeaf3278341
Ron Stropoli <ronstropoli@Rons-MacBook-Pro.local>
2024-11-12 23:33:46 -0500

    added gluestack and tailwind. took sometime to get working need to set export NODE_OPTIONS=--max_old_space_size=4096

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
+import '@/global.css';
 import { Slot, Stack } from 'expo-router';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
 //     return <Stack />;
-    return <Stack />;
+    return (
+        <GluestackUIProvider>
+            <Stack />
+        </GluestackUIProvider>
+
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
+// import "@/global.css";
+import { Button, ButtonText } from '@/components/ui/button';
 import { View, Text } from 'react-native';
+// import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function HomeScreen() {
     return (
-        <View>
-            <Text style={{ fontSize: 30 }}>Hello Ron!!!</Text>
-        </View>
+        // <Text>Hello Ron !!!</Text>
+        <Button variant="outline">
+            <ButtonText>Press Me!!</ButtonText>
+        </Button>
+
     );
 }
